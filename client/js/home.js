46d0b64254320617c8a3a446583d3a44e28de4dd
Nathan Ju <34ndju@gmail.com>
2017-01-20 03:45:12 +0000

    Test sample data

--- client/js/home.js
+++ client/js/home.js
@@ -1,17 +1,38 @@
 $(document).ready(function() {
-    
+
     //setup graph size
     var margin = {top: 30, bottom: 30, right: 50, left: 50};
     var width = 960 - margin.left - margin.right,
         height = 520 - margin.top - margin.bottom;
     
-    //make sure to set the variable names EXACTLY for a and y
-    var xVariable = '',
-        yVariable = '';
+    //make sure to set the variable names EXACTLY for x and y
+    var xVariable = 'x',
+        yVariable = 'y';
     
-    var xValue = function(d) {return d[xVariable]
-        xScale = d3.scale.linear().range(0, width),
-        xMap = function(d) {return 
+    //setup x axis
+    var xValue = function(d) {return d[xVariable]},
+        xScale = d3.scaleLinear().range([0, width]),
+        xMap = function(d) {return xScale(xValue(d))},
+        xAxis = d3.axisBottom(xScale);
     
-    var yScale = d3.scale.linear().range(height, 0)
+    //setup y axis
+    var yValue = function(d) {return d[yVariable]},
+        yScale = d3.scaleLinear().range([height, 0]),
+        yMap = function(d) {return yScale(yValue(d))},
+        yAxis = d3.axisLeft(yScale);
+        
+    //adding canvas to body
+    var svg = d3.select('body').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        
+    //deal with the data; array of JSON objects
+    d3.json('/sampleJSONData', function(err, data) {
+        
+        //adjust the axes
+        xScale.domain([d3.min(data, xValue) - 1, d3.max(data, xValue) + 1]);
+        yScale.domain([d3.min(data, yValue) - 1, d3.max(data, yValue) + 1]);
+    })
 })
--- routes.js
+++ routes.js
@@ -4,4 +4,23 @@ module.exports = function(express, app, bodyParser, path) {
     app.get('/', function(req, res) {
         res.render('home');
     })
+    
+    app.get('/sampleJSONData', function(req, res) {
+        res.send([{
+            x: 34,
+            y: 21
+        },
+        {
+            x: 28,
+            y: 78
+        },
+        {
+            x: 12,
+            y: 79
+        },
+        {
+            x: 90,
+            y: 10
+        }])
+    })
 }
