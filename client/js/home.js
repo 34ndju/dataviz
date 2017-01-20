ca059adb7c082e19d710ad6ebed8d2660ff1a544
Nathan Ju <34ndju@gmail.com>
2017-01-20 04:30:02 +0000

    Test graphing successful

--- client/js/home.js
+++ client/js/home.js
@@ -34,5 +34,39 @@ $(document).ready(function() {
         //adjust the axes
         xScale.domain([d3.min(data, xValue) - 1, d3.max(data, xValue) + 1]);
         yScale.domain([d3.min(data, yValue) - 1, d3.max(data, yValue) + 1]);
+
+        //x axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text(xVariable);
+            
+        // y-axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(yVariable);
+            
+        // draw dots
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 3.5)
+            .attr("cx", xMap)
+            .attr("cy", yMap)
+            .style("fill", "red") 
     })
 })
